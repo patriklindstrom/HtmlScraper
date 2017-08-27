e0600864ab086e8cbd6215b8b0e6c4491c8a4089
Patrik Lindström <patrik.lindstrom@lcube.se>
2017-08-27 20:57:15 +0200

    Simple weekday lunch works

---
+++ HtmlScraper.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HtmlScraper.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HtmlScraper.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82978f00-2d68-466f-b990-9dddabe2978e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- HtmlScraper/Program.cs
+++ HtmlScraper/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace HtmlScraper
 {
     class Program
     {
+        
+        // http://html-agility-pack.net/?z=codeplex
         static void Main(string[] args)
         {
+            // From Web
+
+            //$menu = $(Invoke-WebRequest "https://www.sabis.se/skandia/dagens-lunch/").AllElements | 
+            // Where Class -eq "lunch-day-container" | Select - ExpandProperty innerText
+            var url = "https://www.sabis.se/skandia/dagens-lunch/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var nodes = doc.DocumentNode.SelectNodes("//div[contains(@class, \'lunch-day-container\')]");
+            Debug.Assert(nodes != null, "nodes != null");
+            Dictionary<DayOfWeek, String> weekMenu = new Dictionary<DayOfWeek, string>
+            {
+                [DayOfWeek.Monday] = nodes[0].InnerText,
+                [DayOfWeek.Tuesday] = nodes[1].InnerText,
+                [DayOfWeek.Wednesday] = nodes[2].InnerText,
+                [DayOfWeek.Thursday] = nodes[3].InnerText,
+                [DayOfWeek.Friday] = nodes[4].InnerText
+            };
+            foreach (var lunch in weekMenu)
+            {
+                Console.WriteLine(lunch.Value);
+                Console.WriteLine("");
+            }
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+         Console.WriteLine("Hit any key to exit");
+            Console.ReadKey();
         }
     }
 }
+//  .Where(x => x.Attributes["class"].Value == "box"))
